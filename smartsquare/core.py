a4ab4e71c7b427152089fc8b83057c113f7c198b
mariseco <marisecondo@gmail.com>
2020-10-21 15:08:22 +0200

    Esercitazione del 19 ottobre di computing methods

---
+++ smartsquare/core.py
@@ -0,0 +1,6 @@
+"""
+Faccio una prova
+"""
+def square(x):
+    """ function that return the square """
+    return x**2.
---
+++ tests/test_core.py
@@ -0,0 +1,17 @@
+"""
+    provo test
+"""
+import unittest
+
+from smartsquare.com import square
+
+class TestCore(unittest.TestCase):
+    '''Unittest for core module'''
+ #scritto in Camel case ovvero prima lettera con maiuscola e si separano le lettere con la maiuscola, invece snake case scrivi tutto minuscolo e separi le parole cone un underscore
+    
+    def test_float(self):
+        '''test with floats'''
+        self.assertAlmostEqual(square(2.),4.)
+        
+if __name__ == '__main__':
+    unittest.main()
