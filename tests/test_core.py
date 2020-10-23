c89b2db9e6e176b4ec35bb0c7135a179f8324f7b
mariseco <marisecondo@gmail.com>
2020-10-23 17:47:52 +0200

    Ho corretto un errore in test_core

--- tests/test_core.py
+++ tests/test_core.py
@@ -13,5 +13,5 @@ class TestCore(unittest.TestCase):
         '''test with floats'''
         self.assertAlmostEqual(square(2.),4.)
         
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
